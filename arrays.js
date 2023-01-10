2e51d3cc87f2416e302db277f33ddf9ded27e8d4
btarnow <beckytarnowski@gmail.com>
2023-01-09 20:56:39 -0500

    finished arrays.js and functions.js

--- arrays.js
+++ arrays.js
@@ -1,16 +1,78 @@
 'use strict';
 
+// Python Code: 
+
+// for i in range(len(items)):
+//         print(f'{items[i]} {i}')
+
+// input --> ["apple", berry, cherry]
+// // output -->  apple 0
+//                berry 1
+//                cherry 2
+
+
+//We know to get length it's .length
+//range is range(start, stop)
+
+//for (let i = 0; i < items.length; i += 1) {
+//   console.log(i);
+// }
+
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (let index = 0; index < items.length; index +=1) 
+    console.log(items[index], index)
+  
 }
+
+// printIndices(["apple", "berry", "cherry"])
+
+
+
+// input --> [0, 1, 2, 3, 4, 5]
+// output --> [0, 2, 4]
+
+// add to end with:
+// vowels.push('y');
+
+
+// create an empty array to add the values of the even indicies to 
+// loop through the original array 
+  // if the index is even, add the value of the index to the empty array
+
+// print our the array 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const everyOtherItem = [];
+
+  for (let index = 0; index < items.length; index +=1) {
+    if (index % 2 === 0) {
+      everyOtherItem.push(items[index]);
+      }
+    }
+    console.log(everyOtherItem)
 }
 
+// everyOtherItem([0, 1, 2, 3, 4, 5])
+
+// input --> ([1, 5, 2, 0], 2)
+// output --> [1, 0]
+
+
+// sort the list first in acesending --> [0, 1, 2, 5]
+// shorten it the length of n (2) but it is index 0 and index 1 --> [0, 1]
+// sort the list in decending order --> [1, 0]
+
+  
+
 // 3. smallestNItems
+
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort();
+  const sortedNItems = sortedItems.slice(0, n);
+  sortedNItems.reverse();
+  console.log(sortedNItems);
 }
+
+smallestNItems([1, 5, 2, 0], 2)
--- functions.js
+++ functions.js
@@ -1,13 +1,95 @@
 'use strict';
 
-// 1. isHometown
+//input --> "San Francisco"
+//output --> True 
 
-// Define your function here
+//input --> "Scottsdale"
+//output --> False
+
+// if town is San Fransciso return true, if not return false 
+
+// 1. isHometown
+function isHometown(town) {
+
+    if(town === "San Francisco") {
+        return true; 
+    }
+    else {
+        return false;
+    }
+    
+}
+
+isHometown("Japan")
+
+
+// input --> ("Becky", "Tarnowski")
+// output --> Becky Tarnowski
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+    const fullName = `${firstName} ${lastName}`;
+    console.log(fullName);
+    return fullName;
+}
+
+getFullName("Becky", "Tarnowski")
+
+
+// input --> (1 , 'CA', tax= 0.05)
+    //subtotal --> 1 * (1 + 0.05) 
+    // fee --> 0.03 * 1.05 
+    // total --> 1.05 + 0.32 --> 1.37
+// output --> 1.37
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax = 0.05) {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+
+    if(state === 'CA') {
+        fee = (0.03 * subtotal);
+    }
+    else if(state === 'PA') {
+        fee = 2;
+    }
+    else if(state === 'MA') {
+        if(basePrice <= 100){
+            fee = 1;
+        }
+        else {
+            fee = 3
+        }
+    }
+    
+
+    return subtotal + fee;
+
+}
+
+calculateTotal(1, 'CA')
+
+
+
+
+// // 3. calculateTotal SOLUTION: 
+// function calculateTotal(basePrice, state, tax = 0.05) {
+//     const subtotal = basePrice * (1 + tax);
+  
+//     let fee = 0;
+//     if (state === 'CA') {
+//       fee = 0.03 * subtotal;
+//     } else if (state === 'PA') {
+//       fee = 2;
+//     } else if (state === 'MA') {
+//       if (basePrice <= 100) {
+//         fee = 1;
+//       } else {
+//         fee = 3;
+//       }
+//     }
+  
+//     return subtotal + fee;
+//   }
